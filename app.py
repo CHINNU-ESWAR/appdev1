51b62f027ef7ef219ca226979bdf87c5b3201b3b
Eswar Reddy <84143102+CHINNU-ESWAR@users.noreply.github.com>
2022-12-20 19:52:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 from jinja2 import Template
 from flask import Flask
+TEMPLATE="""hello {{name}}"""
+def main():
+  template=Template(TEMPLATE)
+  print(render.template(name="eswar"))
+if __name__=="__main__":
+  main()
