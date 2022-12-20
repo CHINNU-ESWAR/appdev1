5cc40a740bc6c23f707a7e10b565c06482c15137
Eswar Reddy <84143102+CHINNU-ESWAR@users.noreply.github.com>
2022-12-20 20:18:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from jinja2 import Template
-from flask import Flask
+#from flask import Flask
 data_set=[
-{"year":2000,"awardee":"abcs","state":"andhra]
+{"year":2000,"awardee":"abcs","state":"andhra"},
+  {"year":2001,"awardee":"sgwvg","state":"telangana"},
+  {"year":2002,"awardee":"sbdh","state":"bihar"}]
 def main():
   template_file=open("extemplate.jinja2",'r')
   TEMPLATE=template_file.read()
   template_file.close()
   template=Template(TEMPLATE)
   content=template.render(data_set=data_set)
- jinja2tohtml=open("exjinja2.html","w")
- jinja2tohtml.write(content)
- jinja2tohtml.close()
+  jinja2tohtml=open("exjinja2.html",'w')
+  jinja2tohtml.write(content)
+  jinja2tohtml.close()
 if __name__=="__main__":
   main()
