8821eeb7bcc1354825d349da4286b57b8fb8d165
Eswar Reddy <84143102+CHINNU-ESWAR@users.noreply.github.com>
2022-12-20 20:07:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from jinja2 import Template
 from flask import Flask
-TEMPLATE="""hello {{name}}"""
+data_set=[
+{"year":2000,"awardee":"abcs","state":"andhra]
 def main():
+  template_file=open("extemplate.jinja2",'r')
+  TEMPLATE=template_file.read()
+  template_file.close()
   template=Template(TEMPLATE)
-  print(render.template(name="eswar"))
+  print(template.render(data_set=data_set))
 if __name__=="__main__":
   main()
